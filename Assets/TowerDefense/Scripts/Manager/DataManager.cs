606fea72cf032fc3754682ad02110e8b78d46b1d
taind <dinhtai2000@gmail.com>
2023-07-21 21:47:37 +0700

    b15

---
+++ Assets/TowerDefense/Scripts/Manager/DataManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager Instance;
+    public int Cost = 200;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public int GetCost()
+    {
+        return Cost;
+    }
+    public void AddCost(int value)
+    {
+        Cost += value;
+    }
+    public void SubstractCost(int value)
+    {
+        Cost -= value;
+    }
+}
